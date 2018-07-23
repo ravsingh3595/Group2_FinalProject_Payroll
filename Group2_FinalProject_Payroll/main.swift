e5a0a61190f9310ee201055dbba85588cf0609bc
user <ravneets47@gmail.com>
2018-07-23 18:17:47 -0400

    Initial Commit

---
+++ Group2_FinalProject_Payroll/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Group2_FinalProject_Payroll
+//
+//  Created by user on 2018-07-23.
+//  Copyright © 2018 RavSingh. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
